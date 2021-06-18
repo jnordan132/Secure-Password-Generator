56af053143a0674a55830c93cf0a9b49c4b25054
Jacob Nordan <jacob.nordan113322@gmail.com>
2021-06-18 17:40:34 -0400

    re-did javascript and finished homework

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-## Password Generator 
+# Password Generator 
 
-###
+## Secure-Password-Generator
 
-# Secure-Password-Generator
+This application generates a random password based on user-selected criteria. The application validates user input and ensure that at least one character type is selected. Once all prompts are answered, the user will be presented with a password matching the answered prompts. This application is also responsive, ensuring that it adapts to multiple screen sizes.
---
+++ script.js
@@ -0,0 +1,51 @@
+// Static Variables
+var hasUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var hasUpperArray = hasUpper.split("");
+var hasLower = "abcdefghijklmnopqrstuvwxyz";
+var hasLowerArray = hasLower.split("");
+var hasNum = "0123456789";
+var hasNumArray = hasNum.split("");
+var symbol = "!#$%&()*+,-./:;<=>?@^[]^_`{|}~";
+var symbolArray = symbol.split("");
+
+// Password generation function
+generate.onclick = writePassword();
+
+function writePassword() {
+    var allChars = [];
+    var resultPass = "";
+
+    // Dynamic variables
+    var Totlength = prompt("How many characters would you like your password to be?\n8-128");
+
+    if (Totlength < 8 || Totlength > 128) {
+        alert("Your password needs to be between 8 and 128 characters. Please restart.");
+    }
+    // Confirm password parameters
+    else {
+        if (confirm("Contain upper case letters?")) {
+            Array.prototype.push.apply(allChars, hasUpperArray);
+        }
+        if (confirm("Contain lower case letters?")) {
+            Array.prototype.push.apply(allChars, hasLowerArray);
+        }
+        if (confirm("Contain numbers?")) {
+            Array.prototype.push.apply(allChars, hasNumArray);
+        }
+        if (confirm("Ccontain symbols?")) {
+            Array.prototype.push.apply(allChars, symbolArray);
+        }
+        if (allChars.length === 0) {
+            alert("You must select at lease 1 type of characters to generate a password! Please restart.");
+        }
+        // For loop used to confirm info then generate password
+        else {
+            for (var i = 0; i < Totlength; i++) {
+                var random = Math.floor(Math.random() * allChars.length);
+                resultPass += allChars[random];
+            }
+        }
+    }
+    // Display password
+    document.getElementById("password").innerHTML = resultPass;
+}
